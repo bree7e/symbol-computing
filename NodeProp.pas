e000d5c5507a64f3e666811a6e012c64069ea8c0
Alexandr Vetrov <vetrov@icc.ru>
2018-09-22 17:32:42 +0800

    Убраны поля

--- NodeProp.pas
+++ NodeProp.pas
@@ -9,26 +9,15 @@ uses
 type
   TNodeProperties = class(TForm)
     LabelCaption: TLabel;
-    NodeShape: TRadioGroup;
-    Colors: TGroupBox;
-    Label2: TLabel;
-    Label3: TLabel;
-    NodeBodyColor: TPanel;
-    NodeBorderColor: TPanel;
     btnChangeFont: TButton;
     btnOK: TButton;
     btnCancel: TButton;
     FontDialog: TFontDialog;
-    ColorDialog: TColorDialog;
     NodeText: TMemo;
     btnApply: TButton;
     GroupBox1: TGroupBox;
-    OpenPictureDialog: TOpenPictureDialog;
     cbAlignment: TComboBox;
     AllOptions: TCheckListBox;
-    Label4: TLabel;
-    BodyColor: TShape;
-    BorderColor: TShape;
     cbLayout: TComboBox;
     edtMargin: TEdit;
     UpDownMargin: TUpDown;
@@ -40,10 +29,6 @@ type
     Label8: TLabel;
     BorderStyle: TComboBox;
     Label9: TLabel;
-    GroupBox3: TGroupBox;
-    btnChangBkgnd: TButton;
-    btnClearBackground: TButton;
-    btnBackgroundMargins: TButton;
     procedure NodeBodyColorClick(Sender: TObject);
     procedure NodeBorderColorClick(Sender: TObject);
     procedure btnChangeFontClick(Sender: TObject);
@@ -114,12 +99,12 @@ begin
       end;
       UpDownMargin.Position := Margin;
       NodeText.Lines.Text := Text;
-      if Nodes.Count = 1 then
-        NodeShape.ItemIndex := NodeShape.Items.IndexOfObject(TObject(ClassType))
-      else
-        NodeShape.ItemIndex := -1;
-      BodyColor.Brush.Color := Brush.Color;
-      BorderColor.Brush.Color := Pen.Color;
+//      if Nodes.Count = 1 then
+//        NodeShape.ItemIndex := NodeShape.Items.IndexOfObject(TObject(ClassType))
+//      else
+//        NodeShape.ItemIndex := -1;
+//      BodyColor.Brush.Color := Brush.Color;
+//      BorderColor.Brush.Color := Pen.Color;
       FillStyle.ItemIndex := Ord(Brush.Style);
       BorderStyle.ItemIndex := Ord(Pen.Style);
       FontDialog.Font := Font;
@@ -145,8 +130,8 @@ begin
   for I := 0 to TSimpleGraph.NodeClassCount - 1 do
   begin
     NodeClass := TSimpleGraph.NodeClasses(I);
-    NodeShape.Items.AddObject(PrettyNodeClassName(NodeClass.ClassName),
-      TObject(NodeClass));
+//    NodeShape.Items.AddObject(PrettyNodeClassName(NodeClass.ClassName),
+//      TObject(NodeClass));
   end;
 end;
 
@@ -173,13 +158,13 @@ begin
           end;
           Margin := UpDownMargin.Position;
           Text := NodeText.Lines.Text;
-          Brush.Color := BodyColor.Brush.Color;
-          Pen.Color := BorderColor.Brush.Color;
-          Brush.Style := TBrushStyle(FillStyle.ItemIndex);
-          Pen.Style := TPenStyle(BorderStyle.ItemIndex);
+//          Brush.Color := BodyColor.Brush.Color;
+//          Pen.Color := BorderColor.Brush.Color;
+//          Brush.Style := TBrushStyle(FillStyle.ItemIndex);
+//          Pen.Style := TPenStyle(BorderStyle.ItemIndex);
           Font := FontDialog.Font;
           if Backgnd = 1 then
-            Background.LoadFromFile(OpenPictureDialog.FileName)
+//            Background.LoadFromFile(OpenPictureDialog.FileName)
           else if Backgnd = 2 then
             Background.Graphic := nil;
           BackgroundMargins := MarginRect;
@@ -188,8 +173,8 @@ begin
         finally
           EndUpdate;
         end;
-        if NodeShape.ItemIndex >= 0 then
-          ConvertTo(TSimpleGraph.NodeClasses(NodeShape.ItemIndex));
+//        if NodeShape.ItemIndex >= 0 then
+//          ConvertTo(TSimpleGraph.NodeClasses(NodeShape.ItemIndex));
       end;
   finally
     S.EndUpdate;
@@ -199,16 +184,16 @@ end;
 
 procedure TNodeProperties.NodeBodyColorClick(Sender: TObject);
 begin
-  ColorDialog.Color := BodyColor.Brush.Color;
-  if ColorDialog.Execute then
-    BodyColor.Brush.Color := ColorDialog.Color;
+//  ColorDialog.Color := BodyColor.Brush.Color;
+//  if ColorDialog.Execute then
+//    BodyColor.Brush.Color := ColorDialog.Color;
 end;
 
 procedure TNodeProperties.NodeBorderColorClick(Sender: TObject);
 begin
-  ColorDialog.Color := BorderColor.Brush.Color;
-  if ColorDialog.Execute then
-    BorderColor.Brush.Color := ColorDialog.Color;
+//  ColorDialog.Color := BorderColor.Brush.Color;
+//  if ColorDialog.Execute then
+//    BorderColor.Brush.Color := ColorDialog.Color;
 end;
 
 procedure TNodeProperties.btnChangeFontClick(Sender: TObject);
@@ -218,7 +203,7 @@ end;
 
 procedure TNodeProperties.btnChangBkgndClick(Sender: TObject);
 begin
-  if OpenPictureDialog.Execute then
+//  if OpenPictureDialog.Execute then
     Backgnd := 1;
 end;
 
